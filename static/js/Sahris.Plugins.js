f7b3de4c2292ab289b0100d1a5bf5d2a7b76614f
prologic <prologic@localhost>
2009-07-31 02:30:24 +1000

    Added submit handler as well

--- static/js/Sahris.Plugins.js
+++ static/js/Sahris.Plugins.js
@@ -24,12 +24,22 @@ Sahris.Plugins["html"] = new Class({
     }
 });
 
-var PREFERENCES_TPL = "<div id=\"preferences\" class=\"preferences\"><h2>Preferences</h2><div class=\"system-message\" style=\"display: none;\" /><form method=\"post\" action=\"\" id=\"preferencesForm\"><fieldset><legend>Required</legend><div><label>Signature: <input type=\"text\" name=\"signature\" class=\"textwidget\" size=\"20\" /></label></div></fieldset><div class=\"buttons\"><input id=\"saveBtn\" type=\"button\" value=\"Save\" /></div></form></div>";
+var PREFERENCES_TPL = "<div id=\"preferences\" class=\"preferences\"><h2>Preferences</h2><div class=\"system-message\" style=\"display: none;\" /><form id=\"preferencesForm\" name=\"preferencesForm\" action=\"\" method=\"post\"><fieldset><legend>Required</legend><div><label>Signature: <input type=\"text\" name=\"signature\" class=\"textwidget\" size=\"20\" /></label></div></fieldset><div class=\"buttons\"><input id=\"saveBtn\" type=\"button\" value=\"Save\" /></div></form></div>";
 
 Sahris.Plugins["Preferences"] = new Class({
     Implements: Sahris.Plugin,
 
     el: null,
+
+    save: function() {
+        var uri = new URI(location.href);
+        Cookie.write("signature",
+            this.el.getElement("[name=signature]").get("value"), {
+            domain: uri.parsed.host,
+            duration: 90
+        });
+        this.setMessage(this.el, "Preferences saved");
+    },
 
     run: function (node, data) {
         this.el = $(node);
@@ -38,14 +48,14 @@ Sahris.Plugins["Preferences"] = new Class({
         this.el.getElement("[name=signature]").set("value",
             Cookie.read("signature") || "");
 
+        this.el.getElement("#preferencesForm").on("submit", function (e) {
+            e.preventDefault();
+            this.save();
+        }.bind(this));
+
         this.el.getElement("#saveBtn").on("click", function (e) {
-            var uri = new URI(location.href);
-            Cookie.write("signature",
-                this.el.getElement("[name=signature]").get("value"), {
-                domain: uri.parsed.host,
-                duration: 90
-            });
-            this.setMessage(this.el, "Preferences saved");
+            e.preventDefault();
+            this.save();
         }.bind(this));
     }
 });
