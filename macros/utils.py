29f0f24d0bfba3bf2dd4ffe00a237fbeb22d5e5c
prologic <prologic@localhost>
2010-07-05 00:59:16 +1000

    Initial new version based on toy circuits.web wiki demo

---
+++ macros/utils.py
@@ -0,0 +1,14 @@
+"""Utils macros
+
+Utility macros
+"""
+
+from inspect import getdoc
+
+def macros(macro, environ, *args, **kwargs):
+    """Return a list of available macros"""
+
+    macros = environ["macros"].items()
+    s = "\n".join(["== %s ==\n%s\n" % (k, getdoc(v)) for k, v in macros])
+
+    return environ["parser"].generate(s, environ=environ)
