da6d808f67c5df3cd17bf8fbbf0220b663f09c86
prologic <prologic@localhost>
2010-07-08 04:24:48 +1000

    Fixed plugin support and got HelloWorld plugin to work as /+hello

--- plugins/helloworld.py
+++ plugins/helloworld.py
@@ -2,12 +2,17 @@
 # Author:   James Mills, prologic at shortcircuit dot net dot au
 # Date:     6th February 2010
 
+"""HelloWorld Plugin
+
+...
+"""
+
+from circuits.web import expose
+
 from . import BasePlugin
 
 class HelloWorld(BasePlugin):
 
-    def run(self, *args):
-        pass
-
-    def render(self):
+    @expose("+hello")
+    def hello(self, *args, **kwargs):
         return "Hello World!"
