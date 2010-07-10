553391cddb339607ef75e8a15facb5f4973cb5ac
prologic <prologic@localhost>
2010-07-11 01:12:22 +1000

    Re-adopted hatta's WikiSearch and WikiStorage classes

--- sahriswiki/storage.py
+++ sahriswiki/storage.py
@@ -1,17 +1,15 @@
+import re
 import os
+import thread
 import tempfile
-import mimetypes
+import datetime
 
-os.environ["HGENCODING"] = "utf-8"
-os.environ["HGMERGE"] = "internal:merge"
-
-import mercurial.hg
-import mercurial.ui
-import mercurial.util
-import mercurial.revlog
-from mercurial.node import short
+import mercurial
 
 from circuits.web.utils import url_quote, url_unquote
+
+from i18n import _
+from errors import ForbiddenErr, NotFoundErr
 
 def locked_repo(func):
     """A decorator for locking the repository when calling a method."""
@@ -29,14 +27,6 @@ def locked_repo(func):
 
     return new_func
 
-def page_mime(addr):
-    mime, encoding = mimetypes.guess_type(addr, strict=False)
-    if encoding:
-        mime = 'archive/%s' % encoding
-    if mime is None:
-        mime = 'text/x-wiki'
-    return mime
-
 class WikiStorage(object):
     """
     Provides means of storing wiki pages and keeping track of their
@@ -52,7 +42,7 @@ class WikiStorage(object):
         """
 
         self.charset = charset or 'utf-8'
-        self.path = path
+        self.path = os.path.abspath(path)
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         self.repo_path = self._find_repo_path(self.path)
@@ -71,14 +61,27 @@ class WikiStorage(object):
         else:
             create = False
         self.repo_prefix = self.path[len(self.repo_path):].strip('/')
-        self.repo = mercurial.hg.repository(self.ui, self.repo_path,
-                                            create=create)
+        self._repos = {}
+        # Create the repository if needed.
+        mercurial.hg.repository(self.ui, self.repo_path, create=create)
 
     def reopen(self):
         """Close and reopen the repo, to make sure we are up to date."""
 
-        self.repo = mercurial.hg.repository(self.ui, self.repo_path)
+        #self.repo = mercurial.hg.repository(self.ui, self.repo_path)
+        self._repos = {}
 
+    @property
+    def repo(self):
+        """Keep one open repository per thread."""
+
+        thread_id = thread.get_ident()
+        try:
+            return self._repos[thread_id]
+        except KeyError:
+            repo = mercurial.hg.repository(self.ui, self.repo_path)
+            self._repos[thread_id] = repo
+            return repo
 
     def _find_repo_path(self, path):
         """Go up the directory tree looking for a repository."""
@@ -89,20 +92,48 @@ class WikiStorage(object):
                 return None
         return path
 
+    def _check_path(self, path):
+        """
+        Ensure that the path is within allowed bounds.
+        """
+
+        abspath = os.path.abspath(path)
+        if os.path.islink(path) or os.path.isdir(path):
+            raise ForbiddenErr(
+                _(u"Can't use symbolic links or directories as pages"))
+        if not abspath.startswith(self.path):
+            raise ForbiddenErr(
+                _(u"Can't read or write outside of the pages repository"))
+
     def _file_path(self, title):
-        return os.path.join(self.path, url_quote(title, safe=''))
+        return os.path.join(self.repo_path, self._title_to_file(title))
 
     def _title_to_file(self, title):
-        return os.path.join(self.repo_prefix,
-                            url_quote(title, safe=''))
+        title = unicode(title).strip()
+        filename = url_quote(title, safe='')
+        # Escape special windows filenames and dot files
+        _windows_device_files = ('CON', 'AUX', 'COM1', 'COM2', 'COM3',
+                                 'COM4', 'LPT1', 'LPT2', 'LPT3', 'PRN',
+                                 'NUL')
+        if (filename.split('.')[0].upper() in _windows_device_files or
+            filename.startswith('_') or filename.startswith('.')):
+            filename = '_' + filename
+        return os.path.join(self.repo_prefix, filename)
 
-    def _file_to_title(self, filename):
-        assert filename.startswith(self.repo_prefix)
-        name = filename[len(self.repo_prefix):].strip('/')
+    def _file_to_title(self, filepath):
+        if not filepath.startswith(self.repo_prefix):
+            raise ForbiddenErr(
+                _(u"Can't read or write outside of the pages repository"))
+        name = filepath[len(self.repo_prefix):].strip('/')
+        # Unescape special windows filenames and dot files
+        if name.startswith('_') and len(name)>1:
+            name = name[1:]
         return url_unquote(name)
 
     def __contains__(self, title):
-        return os.path.exists(self._file_path(title))
+        if title:
+            file_path = self._file_path(title)
+            return os.path.isfile(file_path) and not os.path.islink(file_path)
 
     def __iter__(self):
         return self.all_pages()
@@ -111,25 +142,18 @@ class WikiStorage(object):
         """Commits and merges conflicting changes in the repository."""
 
         tip_node = changectx.node()
-        parent_node = self.repo[parent].node()
+        filectx = changectx[repo_file].filectx(parent)
+        parent_node = filectx.changectx().node()
 
         self.repo.dirstate.setparents(parent_node)
         node = self._commit([repo_file], text, user)
 
         partial = lambda filename: repo_file == filename
         try:
-            unresolved = mercurial.merge.update(self.repo, tip_node,
-                                                True, False, partial)
+            mercurial.merge.update(self.repo, tip_node, True, True, partial)
+            msg = _(u'merge of edit conflict')
         except mercurial.util.Abort:
-            unresolved = 1, 1, 1, 1
-        msg = u'merge of edit conflict'
-        if unresolved[3]:
-            msg = u'forced merge of edit conflict'
-            try:
-                mercurial.merge.update(self.repo, tip_node, True, True,
-                                       partial)
-            except mercurial.util.Abort:
-                msg = u'failed merge of edit conflict'
+            msg = _(u'failed merge of edit conflict')
         self.repo.dirstate.setparents(tip_node, node)
         # Mercurial 1.1 and later need updating the merge state
         try:
@@ -142,22 +166,20 @@ class WikiStorage(object):
     def save_file(self, title, file_name, author=u'', comment=u'', parent=None):
         """Save an existing file as specified page."""
 
-        user = author.encode('utf-8') or u'anon'.encode('utf-8')
-        text = comment.encode('utf-8') or u'comment'.encode('utf-8')
+        user = author.encode('utf-8') or _(u'anon').encode('utf-8')
+        text = comment.encode('utf-8') or _(u'comment').encode('utf-8')
         repo_file = self._title_to_file(title)
         file_path = self._file_path(title)
+        self._check_path(file_path)
         mercurial.util.rename(file_name, file_path)
         changectx = self._changectx()
-
         try:
             filectx_tip = changectx[repo_file]
-            current_page_ver = (filectx_tip.filerev(),
-                    short(filectx_tip.node()))
+            current_page_rev = filectx_tip.filerev()
         except mercurial.revlog.LookupError:
             self.repo.add([repo_file])
-            current_page_ver = ()
-
-        if parent is not None and parent not in current_page_ver:
+            current_page_rev = -1
+        if parent is not None and current_page_rev != parent:
             msg = self.merge_changes(changectx, repo_file, text, user, parent)
             user = '<wiki>'
             text = msg.encode('utf-8')
@@ -204,16 +226,12 @@ class WikiStorage(object):
     def page_text(self, title):
         """Read unicode text of a page."""
 
-        page = self.open_page(title)
-        if page:
-            data = page.read()
-            text = unicode(data, self.charset, 'replace')
-            return text
-        else:
-            return None
+        data = self.open_page(title).read()
+        text = unicode(data, self.charset, 'replace')
+        return text
 
     def page_lines(self, page):
-        for data in page:
+        for data in page.xreadlines():
             yield unicode(data, self.charset, 'replace')
 
     @locked_repo
@@ -222,6 +240,7 @@ class WikiStorage(object):
         text = comment.encode('utf-8') or 'deleted'
         repo_file = self._title_to_file(title)
         file_path = self._file_path(title)
+        self._check_path(file_path)
         try:
             os.unlink(file_path)
         except OSError:
@@ -230,10 +249,14 @@ class WikiStorage(object):
         self._commit([repo_file], text, user)
 
     def open_page(self, title):
+        """Open the page and return a file-like object with its contents."""
+
+        file_path = self._file_path(title)
+        self._check_path(file_path)
         try:
-            return open(self._file_path(title), "rb")
+            return open(file_path, "rb")
         except IOError:
-            return None
+            raise NotFoundErr()
 
     def page_file_meta(self, title):
         """Get page's inode number, size and last modification time."""
@@ -250,30 +273,26 @@ class WikiStorage(object):
 
         filectx_tip = self._find_filectx(title)
         if filectx_tip is None:
-            return None
+            raise NotFoundErr()
+            #return -1, None, u'', u''
         rev = filectx_tip.filerev()
-        node = filectx_tip.node()
+        rev = filectx_tip.node()
         filectx = filectx_tip.filectx(rev)
-        date = filectx.date()[0]
+        date = datetime.datetime.fromtimestamp(filectx.date()[0])
         author = unicode(filectx.user(), "utf-8",
                          'replace').split('<')[0].strip()
         comment = unicode(filectx.description(), "utf-8", 'replace')
         return rev, node, date, author, comment
 
     def repo_revision(self):
+        """Give the latest revision of the repository."""
+
         return self._changectx().rev()
 
-    def repo_node(self):
-        return self._changectx().node()
-
-    def page_mime(self, title):
-        """Guess page's mime type ased on corresponding file name."""
-
-        file_path = self._file_path(title)
-        return page_mime(file_path)
 
     def _changectx(self):
         """Get the changectx of the tip."""
+
         try:
             # This is for Mercurial 1.0
             return self.repo.changectx()
@@ -306,25 +325,27 @@ class WikiStorage(object):
         minrev = 0
         for rev in range(maxrev, minrev-1, -1):
             filectx = filectx_tip.filectx(rev)
-            date = filectx.date()[0]
+            date = datetime.datetime.fromtimestamp(filectx.date()[0])
             author = unicode(filectx.user(), "utf-8",
                              'replace').split('<')[0].strip()
             comment = unicode(filectx.description(), "utf-8", 'replace')
             yield rev, date, author, comment
 
     def page_revision(self, title, rev):
-        """Get unicode contents of specified revision of the page."""
+        """Get binary content of the specified revision of the page."""
 
         filectx_tip = self._find_filectx(title)
         if filectx_tip is None:
-            return None
+            raise NotFoundErr()
         try:
             data = filectx_tip.filectx(rev).data()
         except IndexError:
-            return None
+            raise NotFoundErr()
         return data
 
     def revision_text(self, title, rev):
+        """Get unicode text of the specified revision of the page."""
+
         data = self.page_revision(title, rev)
         text = unicode(data, self.charset, 'replace')
         return text
@@ -337,7 +358,7 @@ class WikiStorage(object):
         minrev = 0
         for wiki_rev in range(maxrev, minrev-1, -1):
             change = self.repo.changectx(wiki_rev)
-            date = change.date()[0]
+            date = datetime.datetime.fromtimestamp(change.date()[0])
             author = unicode(change.user(), "utf-8",
                              'replace').split('<')[0].strip()
             comment = unicode(change.description(), "utf-8", 'replace')
@@ -345,26 +366,100 @@ class WikiStorage(object):
                 if repo_file.startswith(self.repo_prefix):
                     title = self._file_to_title(repo_file)
                     try:
-                        version = change[repo_file].filerev()
+                        rev = change[repo_file].filerev()
                     except mercurial.revlog.LookupError:
-                        version = -1
-                    yield title, version, date, wiki_rev, author, comment
+                        rev = -1
+                    yield title, rev, date, author, comment
 
     def all_pages(self):
         """Iterate over the titles of all pages in the wiki."""
 
         for filename in os.listdir(self.path):
-            if (os.path.isfile(os.path.join(self.path, filename))
+            file_path = os.path.join(self.path, filename)
+            if (os.path.isfile(file_path)
+                and not os.path.islink(file_path)
                 and not filename.startswith('.')):
                 yield url_unquote(filename)
 
     def changed_since(self, rev):
         """Return all pages that changed since specified repository revision."""
 
-        last = self.repo.lookup(int(rev))
+        try:
+            last = self.repo.lookup(int(rev))
+        except IndexError:
+            for page in self.all_pages():
+                yield page
+            return
         current = self.repo.lookup('tip')
         status = self.repo.status(current, last)
         modified, added, removed, deleted, unknown, ignored, clean = status
         for filename in modified+added+removed+deleted:
             if filename.startswith(self.repo_prefix):
                 yield self._file_to_title(filename)
+
+
+class WikiSubdirectoryStorage(WikiStorage):
+    """
+    A version of WikiStorage that keeps the subpages in real subdirectories in
+    the filesystem.
+
+    """
+
+    periods_re = re.compile(r'^[.]|(?<=/)[.]')
+    slashes_re = re.compile(r'^[/]|(?<=/)[/]')
+
+    def _title_to_file(self, title):
+        """Modified escaping allowing (some) slashes and spaces."""
+
+        title = unicode(title).strip()
+        escaped = url_quote(title, safe='/ ')
+        escaped = self.periods_re.sub('%2E', escaped)
+        escaped = self.slashes_re.sub('%2F', escaped)
+        path = os.path.join(self.repo_prefix, escaped)
+        return path
+
+    @locked_repo
+    def save_file(self, title, file_name, author=u'', comment=u'', parent=None):
+        """
+        Save the file and make the subdirectories if needed.
+        """
+
+        file_path = self._file_path(title)
+        self._check_path(file_path)
+        dir_path = os.path.dirname(file_path)
+        try:
+            os.makedirs(dir_path)
+        except OSError, e:
+            if e.errno == 17 and not os.path.isdir(dir_path):
+                raise ForbiddenErr(
+                    _(u"Can't make subpages of existing pages"))
+            elif e.errno != 17:
+                raise
+        super(WikiSubdirectoryStorage, self).save_file(title, file_name,
+                                                       author, comment, parent)
+
+    @locked_repo
+    def delete_page(self, title, author=u'', comment=u''):
+        """
+        Remove empty directories after deleting a page.
+
+        Note that Mercurial doesn't track directories, so we don't have to
+        commit after removing empty directories.
+        """
+
+        super(WikiSubdirectoryStorage, self).delete_page(title, author, comment)
+        file_path = self._file_path(title)
+        self._check_path(file_path)
+        dir_path = os.path.dirname(file_path)
+        os.removedirs(dir_path)
+
+    def all_pages(self):
+        """Iterate over the titles of all pages in the wiki. Include subdirectories."""
+
+        for (dirpath, dirnames, filenames) in os.walk(self.path):
+            path = dirpath[len(self.path)+1:]
+            for name in filenames:
+                filename = os.path.join(path, name)
+                if (os.path.isfile(os.path.join(self.path, filename))
+                    and not filename.startswith('.')):
+                    yield url_unquote(filename)
