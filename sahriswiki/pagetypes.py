268e31d71347630ff3a0796e098feff73bb07e25
prologic <prologic@localhost>
2010-07-11 09:39:23 +1000

    Implemented support for text/plain mimetypes (tested)

--- sahriswiki/pagetypes.py
+++ sahriswiki/pagetypes.py
@@ -32,6 +32,9 @@ class WikiPage(object):
     def edit(self):
         raise NotImplemented()
 
+    def history(self):
+        raise NotImplemented()
+
     def view(self):
         raise NotImplemented()
 
@@ -40,6 +43,113 @@ class WikiPageText(WikiPage):
 
 class WikiPageColorText(WikiPageText):
     """Text pages, but displayed colorized with pygments"""
+
+    def _get_text(self):
+        return self.storage.page_text(self.name)
+
+    def _get_page_data(self):
+        text = self._get_text()
+        rev, node, date, author, comment = self.storage.page_meta(self.name)
+
+        data = {
+            "rev": rev,
+            "date": date,
+            "text": text,
+            "author": author,
+            "name": self.name,
+            "comment": comment,
+            "node": short(node),
+            "url": self.url(self.name),
+            "feed": self.url("/+feed/%s" % self.name),
+        }
+
+        self.environ.page = data
+
+        return data
+
+    def download(self):
+        path = self.storage._file_path(self.name)
+        return serve_file(self.request, self.response, path, type=self.mime)
+
+    def edit(self):
+        data = {
+            "actions": [],
+        }
+
+        if not self.request.kwargs:
+            if self.name in self.storage:
+                data["page"] = self._get_page_data()
+                return self.render("edit_plain.html", **data)
+            else:
+                data["page"] = {"name": self.name, "text": ""}
+                return self.render("edit_plain.html", **data)
+
+        author = self.request.cookie.get("username")
+        if author:
+            author = author.value
+        else:
+            author = self.request.headers.get("X-Forwarded-For",
+                    self.request.remote.ip or "AnonymousUser")
+
+        action = self.request.kwargs.get("action", None)
+        comment = self.request.kwargs.get("comment", "")
+        parent = self.request.kwargs.get("parent", None)
+        text = self.request.kwargs.get("text", "")
+
+        if text:
+            text = FIXLINES.sub("\n", text)
+        else:
+            action = "delete"
+
+        if action == "delete":
+            self.storage.reopen()
+            self.search.update(self.environ)
+
+            self.storage.delete_page(self.name, author, comment)
+            self.search.update_page(self, self.name, text=text)
+
+            raise Redirect(self.url("/%s" % self.name))
+        elif action == "cancel":
+            raise Redirect(self.url("/%s" % self.name))
+        elif action == "save":
+            self.storage.reopen()
+            self.search.update(self.environ)
+
+            self.storage.save_text(self.name, text, author, comment,
+                    parent=parent)
+            self.search.update_page(self, self.name, text=text)
+
+            raise Redirect(self.url("/%s" % self.name))
+        else:
+            raise Exception("Invalid action %r" % action)
+
+    def history(self):
+        data = {
+            "actions": [
+                (self.url("/+feed"),                "RSS 1.0"),
+                (self.url("/+feed?format=rss2"),    "RSS 2.0"),
+                (self.url("/+feed?format=atom"),    "Atom"),
+            ],
+            "title": "History of \"%s\"" % self.name,
+            "page": {"name": self.name},
+            "history": self.storage.page_history(self.name),
+            "strftime": strftime,
+            "gmtime": gmtime,
+        }
+
+        return self.render("history.html", **data)
+
+    def view(self):
+        data = {
+            "actions": [
+                (self.url("/+edit/%s" % self.name),     "Edit"),
+                (self.url("/+download/%s" % self.name), "Download"),
+                (self.url("/+history/%s" % self.name),  "History"),
+            ],
+            "page": self._get_page_data()
+        }
+
+        return self.render("view_plain.html", **data)
 
 class WikiPageWiki(WikiPageColorText):
     """Pages of with wiki markup use this for display."""
