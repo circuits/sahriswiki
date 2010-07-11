3c3b3bd9f6d569a0de9dada4c3932c4adae6afc1
prologic <prologic@localhost>
2010-07-11 12:09:34 +1000

    Implemented HTML support

--- sahriswiki/pagetypes.py
+++ sahriswiki/pagetypes.py
@@ -4,6 +4,7 @@ from StringIO import StringIO
 from time import strftime, gmtime
 
 from genshi import Markup
+from genshi.template import Template
 
 from mercurial.node import short
 
@@ -580,8 +581,179 @@ class WikiPageRST(WikiPageText):
 
         return self.render("view_csv.html", **data)
 
-class WikiPageBugs(WikiPageText):
-    """
-    Display class for type text/x-bugs
-    Parse the ISSUES file in (roughly) format used by ciss
-    """
+class WikiPageHTML(WikiPageColorText):
+    """Display HTML (genshi) templates"""
+
+    def _get_text(self):
+        return self.storage.page_text(self.name)
+
+    def _get_page_data(self):
+        text = self._get_text()
+        rev, node, date, author, comment = self.storage.page_meta(self.name)
+
+        data = {
+            "rev": rev,
+            "date": date,
+            "text": text,
+            "author": author,
+            "name": self.name,
+            "comment": comment,
+            "node": short(node),
+            "url": self.url(self.name),
+            "feed": self.url("/+feed/%s" % self.name),
+        }
+
+        return data
+
+    def download(self):
+        path = self.storage._file_path(self.name)
+        return serve_file(self.request, self.response, path, type=self.mime)
+
+    def edit(self):
+        data = {
+            "actions": [],
+        }
+
+        if not self.request.kwargs:
+            if self.name in self.storage:
+                data["page"] = self._get_page_data()
+                return self.render("edit_html.html", **data)
+            else:
+                data["page"] = {"name": self.name, "text": ""}
+                return self.render("edit_html.html", **data)
+
+        author = self.request.cookie.get("username")
+        if author:
+            author = author.value
+        else:
+            author = self.request.headers.get("X-Forwarded-For",
+                    self.request.remote.ip or "AnonymousUser")
+
+        action = self.request.kwargs.get("action", None)
+        comment = self.request.kwargs.get("comment", "")
+        parent = self.request.kwargs.get("parent", None)
+        text = self.request.kwargs.get("text", "")
+
+        if text:
+            text = FIXLINES.sub("\n", text)
+        else:
+            action = "delete"
+
+        if action == "delete":
+            self.storage.reopen()
+            self.search.update(self.environ)
+
+            self.storage.delete_page(self.name, author, comment)
+            self.search.update_page(self, self.name, text=text)
+
+            raise Redirect(self.url("/%s" % self.name))
+        elif action == "cancel":
+            raise Redirect(self.url("/%s" % self.name))
+        elif action == "preview":
+            data["page"] = {"name": self.name, "text": text}
+            data["author"] = author
+            data["comment"] = comment
+            data["preview"] = True
+            data["html"] = Template(text).render(**data)
+            return self.render("edit.html", **data)
+        elif action == "save":
+            self.storage.reopen()
+            self.search.update(self.environ)
+
+            self.storage.save_text(self.name, text, author, comment,
+                    parent=parent)
+            self.search.update_page(self, self.name, text=text)
+
+            raise Redirect(self.url("/%s" % self.name))
+        else:
+            raise Exception("Invalid action %r" % action)
+
+    def history(self):
+        data = {
+            "actions": [
+                (self.url("/+feed"),                "RSS 1.0"),
+                (self.url("/+feed?format=rss2"),    "RSS 2.0"),
+                (self.url("/+feed?format=atom"),    "Atom"),
+            ],
+            "title": "History of \"%s\"" % self.name,
+            "page": {"name": self.name},
+            "history": self.storage.page_history(self.name),
+            "strftime": strftime,
+            "gmtime": gmtime,
+        }
+
+        return self.render("history.html", **data)
+
+    def view(self):
+        data = {
+            "actions": [
+                (self.url("/+edit/%s" % self.name),     "Edit"),
+                (self.url("/+download/%s" % self.name), "Download"),
+                (self.url("/+history/%s" % self.name),  "History"),
+            ],
+            "page": self._get_page_data()
+        }
+
+        data["html"] = Markup(self.render(self.name, **data))
+
+        return self.render("view_html.html", **data)
+
+class WikiPageFile(WikiPage):
+    """Pages of all other mime types use this for display."""
+
+class WikiPageImage(WikiPageFile):
+    """Pages of mime type image/* use this for display."""
+
+    def download(self):
+        path = self.storage._file_path(self.name)
+        return serve_file(self.request, self.response, path, type=self.mime)
+
+    def edit(self):
+        data = {
+            "actions": [],
+        }
+
+        if not self.request.kwargs:
+            raise NotImplemented()
+
+        author = self.request.cookie.get("username")
+        if author:
+            author = author.value
+        else:
+            author = self.request.headers.get("X-Forwarded-For",
+                    self.request.remote.ip or "AnonymousUser")
+
+        action = self.request.kwargs.get("action", None)
+
+        if action == "delete":
+            self.storage.reopen()
+            self.search.update(self.environ)
+
+            self.storage.delete_page(self.name, author, "deleted")
+            self.search.update_page(self, self.name, "")
+
+            raise Redirect(self.url("/%s" % self.name))
+        else:
+            raise Exception("Invalid action %r" % action)
+
+    def view(self):
+        if self.name not in self.storage:
+            data = {
+                "actions": [],
+                "page": {"name": self.name}
+            }
+            return self.render("notfound.html", **data)
+
+        data = {
+            "actions": [
+                (self.url("/+edit/%s?action=delete" % self.name),
+                    "Delete"),
+                (self.url("/+download/%s" % self.name),
+                    "Download"),
+                (self.url("/+upload"),
+                    "Upload"),
+            ],
+        }
+
+        return self.render("view_image.html", **data)
+
