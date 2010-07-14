dcc3a8ea35a00aa4a6fc6c89e3364034f2dc7dc2
prologic <prologic@localhost>
2010-07-15 05:54:33 +1000

    Make our error pages not show tracebacks by default

--- sahriswiki/errors.py
+++ sahriswiki/errors.py
@@ -12,17 +12,29 @@ from circuits.web.exceptions import HTTPException
 class WikiError(HTTPException):
     """Base class for all error pages."""
 
+    traceback = False
+
 class ForbiddenErr(WikiError):
+
     code = 403
 
 class NotFoundErr(WikiError):
+
     code = 404
 
 class UnsupportedMediaTypeErr(WikiError):
+
     code = 415
 
 class NotImplementedErr(WikiError):
+
     code = 501
 
+    description = (
+        "<p>This feature has not yet been implemented."
+        "Please come back and try again later.</p>"
+    )
+
 class ServiceUnavailableErr(WikiError):
+
     code = 503
