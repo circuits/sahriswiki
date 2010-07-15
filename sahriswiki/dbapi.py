e43b3b9990ebf93064ad8913c5c3f0772039f4ff
prologic <prologic@localhost>
2010-07-16 09:10:08 +1000

    Fixed config file processor so that it doesn't add any options in that we don't knwo about (junk)

---
+++ sahriswiki/dbapi.py
@@ -0,0 +1,215 @@
+# Module:   dbapi
+# Date:     12th July 2010
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""DB-API Wrapper Classes
+
+...
+"""
+
+import re
+from itertools import izip as zip
+
+from pyodict import odict
+
+ORACLE_ARRAYSIZE = 2048
+
+def create_connection(s, **kwargs):
+    d = parse_uri(s)
+    schema = d["schema"]
+    username = d["username"]
+    password = d["password"]
+    hostname = d["hostname"]
+    database = d["database"]
+
+    if schema.lower() == "oracle":
+        try:
+            import cx_Oracle as oracle
+        except:
+            raise DriverError("oracle", "No Oracle support available.")
+
+        try:
+            return OracleSession(oracle.connect(
+                dsn=hostname, user=username,
+                password=password), **kwargs)
+        except Exception, e:
+            raise ConnectionError("oracle", e)
+
+    elif schema.lower() == "mysql":
+        try:
+            import MySQLdb as mysql
+        except:
+            raise DriverError("mysql", "No MySQL support available.")
+
+        try:
+            return MySQLSession(mysql.connect(
+                host=hostname,  user=username,
+                passwd=password, db=database), **kwargs)
+        except Exception, e:
+            raise ConnectionError("mysql", e)
+
+    elif schema.lower() == "sqlite":
+        try:
+            import sqlite3 as sqlite
+        except:
+            raise DriverError("sqlite", "No SQLite support available.")
+
+        if database.lower() == ":memory:":
+            filename = ":memory:"
+        else:
+            import os
+            filename = os.path.abspath(
+                    os.path.expanduser(database))
+
+        try:
+            return SQLiteSession(
+                    sqlite.connect(filename), **kwargs)
+        except sqlite.Error, e:
+            raise ConnectionError("sqlite", e)
+
+def parse_uri(s):
+    m = re.match("(?P<schema>oracle|mysql|sqlite)://"
+            "((?P<username>.*?):(?P<password>.*?)@(?P<hostname>.*?)/)?"
+            "(?P<database>.*)",
+            s, re.IGNORECASE)
+    return m.groupdict()
+
+class DriverError(Exception):
+
+    def __init__(self, driver, msg):
+        super(DriverError, self).__init__(msg)
+
+        self.driver = driver
+
+class ConnectionError(Exception):
+
+    def __init__(self, driver, e):
+        super(ConnectionError, self).__init__(e)
+
+        self.driver = driver
+
+class DatabaseError(Exception):
+
+    def __init__(self, sql, e):
+        super(DatabaseError, self).__init__(e)
+
+        self.sql = sql
+
+class BaseSession(object):
+
+    def __init__(self, cx):
+        "initializes x; see x.__class__.__doc__ for signature"
+
+        self._cx = cx
+        self._cu = self.cursor(True)
+
+    def close(self):
+        self._cx.close()
+
+    def rollback(self):
+        self._cx.rollback()
+
+    def _execute(self, sql=None, *args, **kwargs):
+        pass
+
+    def commit(self):
+        self._cx.commit()
+
+    def cursor(self, create=False):
+        if create:
+            return self._cx.cursor()
+        else:
+            return self._cu
+
+    def execute(self, sql=None, *args, **kwargs):
+        try:
+            self._execute(sql, *args, **kwargs)
+            return Records(self, self.getCursor())
+        except Exception, e:
+            raise DatabaseError(sql, e)
+
+    do = execute
+
+class SQLiteSession(BaseSession):
+
+    def _execute(self, sql=None, *args, **kwargs):
+        self._cu.execute(sql, args)
+
+class MySQLSession(BaseSession):
+
+    def _execute(self, sql=None, *args, **kwargs):
+        self._cu.execute(sql, args)
+
+class OracleSession(BaseSession):
+
+    def __init__(self, *args, **kwargs):
+        super(OracleSession, self).__init__(*args, **kwargs)
+
+        self.getCursor().arraysize = ORACLE_ARRAYSIZE
+
+    def _execute(self, sql=None, *args, **kwargs):
+        self._cu.execute(sql, *args, **kwargs)
+
+Connection = create_connection
+
+class Records(object):
+
+    def __init__(self, session, cursor):
+        "initializes x; see x.__class__.__doc__ for signature"
+
+        super(Records, self).__init__()
+
+        self.session = session
+        self.cursor = cursor
+
+        if self.cursor.description is not None:
+            self.fields = [x[0] for x in self.cursor.description]
+        else:
+            self.fields = []
+        
+    def __iter__(self):
+        "x.__iter__() <==> iter(x)"
+
+        return self
+
+    def __len__(self):
+        "x.__len__() <==> len(x)"
+
+        return self.cursor.rowcount
+
+    def next(self):
+        "x.next() -> the next value, or raise StopIteration"
+
+        row = self.cursor.fetchone()
+        if row:
+            return Record(self, zip(self.fields, row))
+        else:
+            raise StopIteration
+
+class Record(odict):
+
+    def __init__(self, records, data):
+        "initializes x; see x.__class__.__doc__ for signature"
+
+        super(Record, self).__init__()
+
+        self.records = records
+
+        for k, v in data:
+            self.add(k, v)
+    
+    def add(self, k, v):
+        if type(k) == tuple:
+            k = k[0]
+
+        if isinstance(v, self.records.cursor.__class__):
+            if self.records.cursor.description is not None:
+                fields = map(lambda x: x[0], v.description)
+                v = Records(self.records, v)
+
+        self[k] = v
+        setattr(self, k, v)
+
+    def remove(self, k):
+        del self[k]
+        delattr(self, k)
---
+++ sahriswiki/env.py
@@ -0,0 +1,235 @@
+# Module:   env
+# Date:     12th July 2010
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""Environment Container
+
+...
+"""
+
+import os
+from hashlib import md5
+from urllib import basejoin
+from itertools import chain
+from urlparse import urlparse
+
+from circuits import handler, BaseComponent
+
+from genshi import Markup
+from genshi.template import TemplateLoader
+
+from creoleparser import create_dialect, creole11_base, Parser
+
+import macros
+import sahriswiki
+from utils import page_mime
+from search import WikiSearch
+from storage import WikiSubdirectoryIndexesStorage as DefaultStorage
+
+from pagetypes import WikiPageHello, WikiPageLogin
+from pagetypes import WikiPageText, WikiPageHTML, WikiPageImage
+from pagetypes import WikiPageWiki, WikiPageFile, WikiPageLogout
+from pagetypes import WikiPageColorText, WikiPageCSV, WikiPageRST
+
+class Environment(BaseComponent):
+
+    filename_map = {
+        "README":       (WikiPageText,  "text/plain"),
+        "COPYING":      (WikiPageText,  "text/plain"),
+        "CHANGES":      (WikiPageText,  "text/plain"),
+        "MANIFEST":     (WikiPageText,  "text/plain"),
+        "favicon.ico":  (WikiPageImage, "image/x-icon"),
+    }
+
+    mime_map = {
+        "text":                     WikiPageText,
+        "application/x-javascript": WikiPageColorText,
+        "application/x-python":     WikiPageColorText,
+        "text/csv":                 WikiPageCSV,
+        "text/html":                WikiPageHTML,
+        "text/x-rst":               WikiPageRST,
+        "text/x-wiki":              WikiPageWiki,
+        "image":                    WikiPageImage,
+        "":                         WikiPageFile,
+        "type/hello":               WikiPageHello,
+        "type/login":               WikiPageLogin,
+        "type/logout":              WikiPageLogout,
+    }
+
+    def __init__(self, config):
+        super(Environment, self).__init__()
+
+        self.config = config
+
+        self.storage = DefaultStorage(
+            self.config.get("data"),
+            self.config.get("encoding"),
+            index=self.config.get("index"),
+            indexes=self.config.get("indexes"),
+        )
+
+        self.search = WikiSearch(
+            self.config.get("cache"),
+            self.config.get("language"),
+            self.storage,
+        )
+
+        self.parser = Parser(
+            create_dialect(
+                creole11_base,
+                macro_func=macros.dispatcher,
+                wiki_links_base_url="/",
+                wiki_links_class_func=self._wiki_links_class_func,
+                wiki_links_path_func=self._wiki_links_path_func,
+            ),
+            method="xhtml"
+        )
+
+        template_config = {
+            "allow_exec": False,
+            "auto_reload": True,
+            "default_encoding": self.config.get("encoding"),
+            "search_path": [
+                self.storage.path,
+                os.path.join(self.config.get("theme"), "templates"),
+            ],
+            "variable_lookup": "lenient",
+        }
+            
+        self.templates = TemplateLoader(**template_config)
+
+        self.macros = macros.loadMacros()
+
+        self.stylesheets = []
+        self.version = sahriswiki.__version__
+
+        self.site = {
+            "name": self.config.get("name"),
+            "author": self.config.get("author"),
+            "keywords": self.config.get("keywords"),
+            "description": self.config.get("description"),
+        }
+
+        self.users = self._create_users()
+
+        self.request = None
+        self.response = None
+
+    def _login(self):
+        return self.request.session.get("login", self.request.login)
+
+    def _user(self):
+        return self._login() or self.request.headers.get(
+                "X-Forwarded-For", self.request.remote.ip)
+
+    def _permissions(self):
+        if self._login():
+            yield "PAGE_EDIT"
+
+    def _nav(self):
+        yield
+
+    def _metanav(self):
+        if not self._login():
+            yield ("Login", self.url("/+login"))
+        else:
+            yield ("Logout", self.url("/+logout"))
+
+        yield ("Preferences", self.url("/+prefs"))
+        yield ("Help/Guide",  self.url("/Help"))
+        yield ("About",       self.url("/+about"))
+
+    def _ctxnav(self, type="view"):
+        if type in ("history", "index", "search"):
+            yield ("Index",    self.url("/+search"))
+            yield ("Orphaned", self.url("/+orphaned"))
+            yield ("Wanted",   self.url("/+wanted"))
+
+    def _create_users(self):
+        users = {"admin": md5(self.config.get("password")).hexdigest()}
+        htpasswd = self.config.get("htpasswd", None)
+        if htpasswd:
+            f = open(htpasswd, "r")
+            for line in f:
+                line = line.strip()
+                if line:
+                    username, password = line.split(":")
+                    users["username"] = password
+            f.close()
+        return users
+
+    def _wiki_links_class_func(self, type, url, body, name):
+        if type == "wiki" and name:
+            if name in self.storage:
+                return "wiki"
+            else:
+                return "wiki new"
+        elif type == "url":
+            base = urlparse(self.url("/"))
+            link = urlparse(url)
+            if not all([base[i] == link[i] for i in range(2)]):
+                return "external"
+
+    def _wiki_links_path_func(self, tag, path):
+        if tag == "img":
+            return self.url("/+download", path)
+        else:
+            return path
+
+    def url(self, *args):
+        return self.request.url("/".join(args))
+
+    def staticurl(self, url):
+        base = self.config.get("static-baseurl", None)
+        if base:
+            return basejoin(base, url)
+        else:
+            return self.request.url("/%s" % url)
+
+    def get_page(self, name):
+        """Creates a page object based on page"s mime type"""
+
+        try:
+            page_class, mime = self.filename_map[name]
+        except KeyError:
+            mime = page_mime(name)
+            major, minor = mime.split("/", 1)
+            try:
+                page_class = self.mime_map[mime]
+            except KeyError:
+                try:
+                    page_class = self.mime_map[major]
+                except KeyError:
+                    page_class = self.mime_map[""]
+
+        return page_class(self, name, mime)
+
+    def include(self, name, context=None):
+        if name in self.storage:
+            return self.parser.generate(self.storage.page_text(name),
+                environ=(self, context))
+        else:
+            return Markup("<!-- SiteMenu Not Found -->")
+
+    def render(self, template, **data):
+        data.update({
+            "sahriswiki": {
+                "version": sahriswiki.__version__
+            },
+            "url":         self.url,
+            "site":        self.site,
+            "config":      self.config,
+            "include":     self.include,
+            "staticurl":   self.staticurl,
+            "permissions": self._permissions(),
+            "nav":         chain(self._nav(), data.get("nav", [])),
+            "crxnav":      chain(self._ctxnav(), data.get("ctxnav", [])),
+            "metanav":     chain(self._metanav(), data.get("metanav", [])),
+        })
+        t = self.templates.load(template)
+        return t.generate(**data).render("xhtml", doctype="html")
+
+    @handler("request", priority=1.0, target="web")
+    def _on_request(self, request, response):
+        self.request = request
+        self.response = response
